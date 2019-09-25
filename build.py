0bd32c8f41d896bed942b9f004d3a85447658949
Morris Hafner <morris.hafner@codeplay.com>
2019-09-25 16:46:58 +0100

    Disable shared builds on Windows platforms in build.py

--- build.py
+++ build.py
@@ -8,4 +8,8 @@ from bincrafters import build_template_default
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
+    if platform.system() == "Windows":
+        builder.add_common_builds(shared_option_name=False)
+    else:
+        builder.add_common_builds()
     builder.run()
